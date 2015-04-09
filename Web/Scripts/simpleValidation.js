f3db3a04ea4ab23631f2c53d179d7876a50532a9
sebastianbadea <sebastian.badea@gmail.com>
2015-04-09 11:11:28 +0300

    add the validation file

---
+++ Web/Scripts/simpleValidation.js
@@ -0,0 +1,14 @@
+﻿$("form").validate({
+    submitHandler: function (form) {
+        form.submit();
+    },
+    //here you can define the rules(you can find all the built-in rules at http://jqueryvalidation.org/documentation#link-list-of-built-in-validation-methods)
+    rules: {
+        Name: {
+            required: true
+        },
+        Code: {
+            required: true
+        }
+    }
+});
