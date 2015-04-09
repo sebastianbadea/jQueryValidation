7a7631a5e87c166307261d7cf8b7a18911f69419
sebastianbadea <sebastian.badea@gmail.com>
2015-04-09 14:47:40 +0300

    defaults

---
+++ Web/Scripts/valDefaults.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    //the default settings can be overriden before apply the validation
+    $.validator.setDefaults({
+        errorElement: "small", // contain the error msg in a small tag
+        wrapper: "div", // wrap the error message and small tag in a div
+        highlight: function (element) {
+            $(element).closest('.form-group').addClass('has-error'); // add the Bootstrap has-error class to the parent div
+        },
+        success: function (element) {
+            $(element).closest('.form-group').removeClass('has-error'); // remove the Boostrap has-error class from the parent div
+        }
+    });
+});
