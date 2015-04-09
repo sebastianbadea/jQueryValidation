dc44e8d06b2c8d76b941e8f3d50d8dd3154f4adf
sebastianbadea <sebastian.badea@gmail.com>
2015-04-09 10:55:22 +0300

    change some names and stuff

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult SimpleValidation()
         {
             return View();
         }
 
-        public void Save()
+        [HttpPost]
+        public ActionResult SimpleValidation(Provider provider)
         {
-            
+            ViewBag.Result = "Result returned from the server";
+            return View(provider);
         }
     }
 }
